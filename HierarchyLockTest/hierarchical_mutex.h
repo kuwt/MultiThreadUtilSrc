26a851160864ef707b83384677ba72a4b275f86b
kuwingto <kuwingto@gmail.com>
2018-07-09 18:05:24 +0800

    add threadsafe_stack

--- HierarchyLockTest/hierarchical_mutex.h
+++ HierarchyLockTest/hierarchical_mutex.h
@@ -42,16 +42,12 @@ public:
 		check_for_hierarchy_violation();
 		if (!internal_mutex.try_lock())
 		{
-
+			return false;
 		}
-
-
+		update_hierarchy_value();
+		return true;
 	}
-
-
-
-
-
+	thread_local unsigned long hierarchical_mutex::this_thread_hierarchy_value(ULONG_MAX);
 
 
 };
---
+++ ThreadSafeStack/threadsafe_stackTest.cpp
@@ -0,0 +1,11 @@
+#include "threadsafe_stack.h"
+
+
+
+
+
+int main()
+{
+    return 0;
+}
+
